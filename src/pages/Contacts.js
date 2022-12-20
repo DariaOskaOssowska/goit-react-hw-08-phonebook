755e01a79bdb818c704c5c2f71bb989ed4461d2a
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-12-21 00:03:36 +0100

    Added filter

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,6 +5,7 @@ import { ContactForm } from 'components/ContactForm/ContactForm';
 import { getIsLoading } from 'redux/contacts/selectors';
 import { ContactList } from 'components/ContactList/ContactList';
 import { fetchContacts } from 'redux/contacts/operations';
+import { Filter } from 'components/Filter/Filter';
 
 export default function Contacts() {
   const dispatch = useDispatch();
@@ -21,6 +22,7 @@ export default function Contacts() {
       </Helmet>
       <ContactForm />
       <div>{isLoading && 'Request in progress...'}</div>
+      <Filter />
       <ContactList />
     </>
   );
